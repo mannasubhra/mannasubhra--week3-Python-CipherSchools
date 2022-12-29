5846fb4533c289808a42fce89add1c7305fe5764
mannasubhra <115398410+mannasubhra@users.noreply.github.com>
2022-12-29 23:42:28 +0530

    Create class159.py

---
+++ class159.py
@@ -0,0 +1,10 @@
+# zip function
+user_id = ['user1', 'user2', 'user3']
+names = ['subhradeep', 'sourya','rajeev']
+last_names = ['manna', 'manna','singh']
+
+# ('user1','subhradeep'),('user2','sourya')
+print(list(zip(user_id, names, last_names)))
+
+# example =[('a',1), ('b', 2)]
+# print(dict(example))
